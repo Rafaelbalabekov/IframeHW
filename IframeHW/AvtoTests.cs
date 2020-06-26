56619536f90fc749a34a54140e37f924509797b2
Rafaelbalabekov <rafael.balabekov.1996@gmail.com>
2020-06-26 14:53:02 +0400

    Добавьте файлы проекта.

---
+++ IframeHW/AvtoTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IframeHW
+{
+    class AvtoTests
+    {
+        Resualts Resualts;
+        IWebDriver driver;
+
+        public AvtoTests()
+        {
+            Resualts = new Resualts();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("http://someurl.com/");
+        }
+        [Test]
+
+        public void Test1()
+        {
+            IWebElement frame1 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame1);
+
+            driver.FindElement(By.XPath("//*[@id='adBlock']/div[2]/div[1]/a")).Click();
+
+            IWebElement frame2 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame2);
+
+            driver.FindElement(By.XPath("//*[@id='adBlock']/div[2]/div[2]/a")).Click();
+
+            Assert.IsTrue(Resualts.Resualt1());
+          
+        }
+
+        [Test]
+
+        public void Test2()
+        {
+            IWebElement frame1 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame1);
+
+            driver.FindElement(By.XPath("//*[@id='adBlock']/div[2]/div[2]/a")).Click();
+
+            IWebElement frame2 = driver.FindElement(By.XPath("//iframe[@id='master-1']"));
+            driver.SwitchTo().Frame(frame2);
+
+            driver.FindElement(By.XPath("//*[@id='e2']/div[4]/div/div/a")).Click();
+
+            Assert.IsTrue(Resualts.Resualt2());
+        }
+
+       [TearDown]
+      
+       public void TearDown()
+       {
+           driver.Quit();
+       }
+    }
+}
---
+++ IframeHW/Resualts.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IframeHW
+{
+    class Resualts
+    {
+        IWebDriver driver;
+
+        public bool Resualt1()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://designstreet.az/?gclid=EAIaIQobChMIgom5jpmf6gIVS7DtCh37kgQDEAEYASAAEgIMm_D_BwE");
+
+            if (driver.FindElements(By.XPath("//*[text()='XİDMƏTLƏR']")).Count != 1)
+            {
+                driver.Quit();
+                return false;
+
+            }
+            else
+            {
+                driver.Quit();
+                return true;
+            }
+        }
+            public bool Resualt2()
+            {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.alibabacloud.com/campaign/free-trial?utm_content=se_1005982031&gclid=EAIaIQobChMIpIb-7aWf6gIVBBUYCh3ugQIQEAEYASAAEgLX0PD_BwE");
+
+                if (driver.FindElements(By.XPath("//*[text()='Alibaba Cloud Free Trial']")).Count != 1)
+                {
+                    driver.Quit();
+                    return false;
+
+                }
+                else
+                {
+                    driver.Quit();
+                    return true;
+                }
+
+            }
+      } } 
